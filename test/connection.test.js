82dc2d5fb02b10d1837740562917b0a54f12c317
arieansyah <arieansyahp.bun@gmail.com>
2019-01-31 14:03:22 +0700

    testing

--- test/connection.test.js
+++ test/connection.test.js
@@ -1,12 +1,12 @@
-// 'use strict'
-// const expect = require('chai').expect
+'use strict'
+const expect = require('chai').expect
 
-// describe('Test Connection', () => {
-//     describe('Test Connection Cassandra', () => {
-//         it('Connection should OK!', () => {
-//             const client = require('../config/connection')
-//             expect(client).to.be.a("object")
-//         })
-//     })
-// })
+describe('Test Connection', () => {
+    describe('Test Connection Cassandra', () => {
+        it('Connection should OK!', () => {
+            const client = require('../config/connection')
+            expect(client).to.be.a("object")
+        })
+    })
+})
 
--- test/siswa.test.js
+++ test/siswa.test.js
@@ -1,94 +1,94 @@
-// let Book = require('../controller/siswaController');
+let Book = require('../controller/siswaController');
 
-// let chai = require('chai');
-// let chaiHttp = require('chai-http');
-// let server = require('../app');
-// var should = chai.should();
-// const client = require('../config/connection')
-
-
-
-// chai.use(chaiHttp);
-
-//   describe('/DROP SISWA', () => {
-//     it('Migrate Table and index table userAccount', (done) => {
-//       client.execute('DROP TABLE siswa')
-//       .then(success => {
-//           chai.request(server)
-//               .post('/migration/siswa')
-//               .end((err, res) => {
-//                   res.should.have.status(200);
-//                   res.body.should.be.a('object');
-//                   res.body.should.have.property('success');
-//                   res.body.should.have.property('success').eql(true);
-//                   res.body.should.have.property('msg');
-//                   chai.request(server)
-//                   .post('/migration/siswa-index')
-//                   .end((err, res) => {
-//                     res.should.have.status(200);
-//                     res.body.should.be.a('object');
-//                     res.body.should.have.property('success');
-//                     res.body.should.have.property('success').eql(true);
-//                     res.body.should.have.property('msg');
-//                     done()
-//                   });
-//           });
-//       })
-//       .catch(err => {
-//           should.throw()
-//           done()
-//       });
-//     });
-//   });
-
-//   describe('/GET', () => {
-//       it('GET Siswa', (done) => {
-//         chai.request(server)
-//             .get('/siswa/data')
-//             .end((err, res) => {
-//                 should.exist(res.body);
-//                 res.should.have.status(200);
-//               done();
-//             });
-//       });
-//   });
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app');
+var should = chai.should();
+const client = require('../config/connection')
 
 
-//   describe('/STORE', () => {
-//         it('CREATE Siswa', (done) => {
-//             let siswa = {
-//                 nama: "ujang",
-//                 umur: "oke"
-//             }
-//         chai.request(server)
-//             .post('/siswa/store')
-//             .send(siswa)
-//             .end((err, res) => {
-//               should.exist(res.body);                    
-//               res.should.have.status(200);
-//               done();
-//             });
-//         });
-//     });
 
-//   describe('/UPDATE', () => {
-//     it('/Update Siswa', function(done) {
-//       chai.request(server)
-//         .get('/siswa/data')
-//         .end(function(err, res){
-//           let siswa = {
-//             id: res.body.siswaRequest[0].id,
-//             nama: "asjdasjd",
-//             umur: "aksdkasdaskjdkas"
-//         }
-//           chai.request(server)
-//             .post('/siswa/update')
-//             .send(siswa)
-//             .end(function(error, res){
-//               should.exist(res.body);                    
-//               res.should.have.status(200);
-//               done();
-//           });
-//         });
-//     });
-// });
+chai.use(chaiHttp);
+
+  describe('/DROP SISWA', () => {
+    it('Migrate Table and index table userAccount', (done) => {
+      client.execute('DROP TABLE siswa')
+      .then(success => {
+          chai.request(server)
+              .post('/migration/siswa')
+              .end((err, res) => {
+                  res.should.have.status(200);
+                  res.body.should.be.a('object');
+                  res.body.should.have.property('success');
+                  res.body.should.have.property('success').eql(true);
+                  res.body.should.have.property('msg');
+                  chai.request(server)
+                  .post('/migration/siswa-index')
+                  .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('success');
+                    res.body.should.have.property('success').eql(true);
+                    res.body.should.have.property('msg');
+                    done()
+                  });
+          });
+      })
+      .catch(err => {
+          should.throw()
+          done()
+      });
+    });
+  });
+
+  describe('/GET', () => {
+      it('GET Siswa', (done) => {
+        chai.request(server)
+            .get('/siswa/data')
+            .end((err, res) => {
+                should.exist(res.body);
+                res.should.have.status(200);
+              done();
+            });
+      });
+  });
+
+
+  describe('/STORE', () => {
+        it('CREATE Siswa', (done) => {
+            let siswa = {
+                nama: "ujang",
+                umur: "oke"
+            }
+        chai.request(server)
+            .post('/siswa/store')
+            .send(siswa)
+            .end((err, res) => {
+              should.exist(res.body);                    
+              res.should.have.status(200);
+              done();
+            });
+        });
+    });
+
+  describe('/UPDATE', () => {
+    it('/Update Siswa', function(done) {
+      chai.request(server)
+        .get('/siswa/data')
+        .end(function(err, res){
+          let siswa = {
+            id: res.body.siswaRequest[0].id,
+            nama: "asjdasjd",
+            umur: "aksdkasdaskjdkas"
+        }
+          chai.request(server)
+            .post('/siswa/update')
+            .send(siswa)
+            .end(function(error, res){
+              should.exist(res.body);                    
+              res.should.have.status(200);
+              done();
+          });
+        });
+    });
+});
